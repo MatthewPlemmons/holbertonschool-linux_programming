c024f4939c3721f1639fba1cca17fd6387b5698d
MatthewPlemmons <matthewleeplemmons@gmail.com>
2018-02-11 03:33:36 +0000

    broke _getline into smaller functions

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -1,34 +1,83 @@
 #include "_getline.h"
 
+
 /**
- * _getline - Read line to a buffer.
+ * read_into_buffer - Read from fd into buffer.
+ * @fd: file descriptor
+ * @buf: buffer
+ * Return: 0 if successful, otherwise -1.
+ */
+int read_into_buffer(const int fd, char *buf)
+{
+	ssize_t len;
+	char *ptr;
+
+	while ((len = read(fd, buf, READ_SIZE)) > 0)
+	{
+		if (len == -1)
+		{
+			free(buf);
+			return (-1);
+		}
+		if (len == READ_SIZE)
+		{
+			ptr = realloc(buf, READ_SIZE * 2 + 1);
+			if (!ptr)
+			{
+				free(buf);
+				return (-1);
+			}
+		}
+	}
+	return (0);
+}
+
+
+/**
+ * _getline - Read from fd and return lines.
  *
  * @fd: file descriptor
  * Return: Null-terminated string, or NULL on failure.
  */
 char *_getline(const int fd)
 {
-	ssize_t len;
-	char *buf;
+	size_t i;
+	static size_t cur_len;
+	static char *buf;
+	char *ptr;
 
-	buf = malloc(sizeof(char) * READ_SIZE);
 	if (!buf)
 	{
-		free(buf);
-		return (NULL);
+		buf = malloc(sizeof(char) * READ_SIZE);
+		if (!buf)
+		{
+			/* factor out these two lines in a free_mem func */
+			free(buf);
+			return (NULL);
+		}
+
+		/* read all bytes from fd and store them in buffer */
+		/* re-size buffer if needed */
+
+		if ((read_into_buffer(fd, buf)) == -1)
+			return (NULL);
 	}
 
-	len = read(fd, buf, READ_SIZE);
-	if (len == -1)
+	/* find and return lines */
+	for (i = 0; buf[i]; ++i)
 	{
-		free(buf);
-		return (NULL);
-	}
-	if (len == 0)
-	{
-		free(buf);
-		return (NULL);
+		if (buf[i] == '\n')
+		{
+			cur_len += i + 1;
+			ptr = malloc(sizeof(char) * i + 1);
+			memcpy(ptr, buf, i);
+			buf += i + 1;
+			ptr[i] = '\0';
+			return (ptr);
+		}
 	}
 
-	return (buf);
+	buf -= cur_len;
+	free(buf);
+	return (NULL);
 }
