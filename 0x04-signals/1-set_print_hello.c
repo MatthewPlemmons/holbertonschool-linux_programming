6f20359863e24102b33b3e22634507cef9553758
MatthewPlemmons <matthewleeplemmons@gmail.com>
2018-03-24 01:26:33 +0000

    separated signal handling functions into new file

---
+++ 0x04-signals/1-set_print_hello.c
@@ -0,0 +1,23 @@
+#include "signals.h"
+
+/**
+ * print_hello - Handle the SIGINT signal.
+ *
+ * @signum: integer value of the signal1
+ */
+void print_hello(int signum)
+{
+        signal(signum, SIG_IGN);
+        printf("Hello :)\n");
+        signal(signum, print_hello);
+}
+
+/**
+ * set_print_hello - Handle received signal.
+ *
+ * Return: 0 on success, -1 on error.
+ */
+void set_print_hello(void)
+{
+        signal(SIGINT, print_hello);
+}
