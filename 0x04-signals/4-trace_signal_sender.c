f3f55faa52c2d8fbe8cb731e31efecec1fe95fe9
MatthewPlemmons <matthewleeplemmons@gmail.com>
2018-03-27 03:56:52 +0000

    update header file and task 4 files

---
+++ 0x04-signals/4-trace_signal_sender.c
@@ -0,0 +1,29 @@
+#include "signals.h"
+
+/**
+ * sigint_handler - Print PID of signal sender.
+ *
+ * @signum: integer value of the signal
+ */
+void sigquit_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) sig, (void) ucontext;
+	printf("SIGQUIT sent by %i\n", info->si_pid);
+}
+
+/**
+ * handle_sigaction - Handle received signal.
+ *
+ * Return: 0 on success, -1 on error.
+ */
+int trace_signal_sender(void)
+{
+	struct sigaction sa;
+
+	sa.sa_sigaction = sigquit_handler;
+	sa.sa_flags = SA_SIGINFO;
+
+	if (sigaction(SIGQUIT, &sa, NULL) < 0)
+		return (-1);
+	return (0);
+}
