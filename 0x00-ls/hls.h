7a9dadc88d1c15aaf372fdd42b641f04376185fb
MatthewPlemmons <matthewleeplemmons@gmail.com>
2018-12-13 20:45:01 +0000

    update header file

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -14,9 +14,11 @@
 
 #define FILE_COUNT 50
 #define MAX_FILENAME_SIZE 256
+#define MAX_PATH_SIZE 4096
 
 /**
- * enum print_mode - keeps track of option flags for printing directory contents.
+ * enum print_mode - keeps track of option flags for printing directory
+ * contents.
  *
  * @print_all: prints all directory contents
  * @almost_all: prints all directory contents but ignores `.` and `..`
@@ -39,28 +41,43 @@ enum format
 	long_format = 2
 };
 
-void print_type(struct stat buf);
-void print_permissions(struct stat buf);
-void print_long_format(const char *file, char *path);
-void print_files(const char **items, char *dir_path, enum format format);
-int print_path_name(char *path);
-/*void print_file_name(char *file); */
-/*int print_items(const char **items);*/
-
+/* hls.c */
+void print_error(const char *s);
+size_t count_files(char *path, enum print_mode print_mode);
 const char **collect_names(char *items,
 			   size_t file_count,
 			   enum print_mode print_mode);
 int free_ptr_array(const char **arr, size_t file_count);
 
+/* hls-print.c */
+void print_type(struct stat buf);
+void print_permissions(struct stat buf);
+void print_long_format(const char *file, char *path);
+void print_files(const char **items, char *dir_path, enum format format);
+int print_path_name(char *path);
+
+/* hls-sort.c */
 int sort_items(const char **items);
 int a_sort(const char *a, const char *b);
 
-char **parse_args(size_t argc, char **argv);
-int check_flags(char *flags, enum format *format, enum print_mode *print_mode);
-int flag_count(char **argv, enum format *format, enum print_mode *print_mode);
+/* hls-arg-parse.c */
+char **extract_directory_paths(size_t argc, char **argv);
+size_t check_flags(char *flags, enum format *format,
+		   enum print_mode *print_mode);
+size_t check_valid_directory(char *path);
+size_t parse_args(char **argv, enum format *format,
+		  enum print_mode *print_mode);
 
+/* hls-dateformat.c */
+char *date_format(char *time);
+
+/* hls-strings.c */
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+
+/* hls-calloc.c */
+void *_calloc(size_t nmemb, size_t size);
 
 #endif
